69ce66f9e333845ab2f0aa576980593bde56e387
kaganuk <kagan@Kagan-MacBook-Pro.local>
2017-01-15 16:37:07 +0300

    Swift İle Sayı Okuma

---
+++ SayiOkuma.swift
@@ -0,0 +1,9 @@
+//  Created by Kağan on 15.01.2017.
+//  Copyright © 2017 Kağan Utku Kılıçlı. All rights reserved.
+
+import Foundation
+
+print("bir sayi giriniz:")
+var sayi = ""
+sayi = readLine(strippingNewline: true)!
+print("girdiginiz sayi: \(sayi)")
