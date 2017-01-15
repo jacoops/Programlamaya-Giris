e679d3e8bc937aadfa064ea86e72cb6bd5d82ef4
kaganuk <kagankilicli@gmail.com>
2017-01-15 15:39:19 +0200

    Delete SayiOkuma.swift

--- SayiOkuma.swift
+++
@@ -1,9 +0,0 @@
-//  Created by Kağan on 15.01.2017.
-//  Copyright © 2017 Kağan Utku Kılıçlı. All rights reserved.
-
-import Foundation
-
-print("bir sayi giriniz:")
-var sayi = ""
-sayi = readLine(strippingNewline: true)!
-print("girdiginiz sayi: \(sayi)")
